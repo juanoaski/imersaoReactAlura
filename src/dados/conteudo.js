ddd7afdd5daa7e61fa040115f052c7fa080fdc46
Juan Oaski Pegoretti <juan.opegoretti@outlook.com>
2020-07-27 20:08:45 -0300

    Alteração de Dados

--- src/dados/conteudo.js
+++ src/dados/conteudo.js
@@ -1,5 +1,39 @@
 export default [
     {
+        nome: 'Marco Bruno',
+        foto: 'https://avatars2.githubusercontent.com/u/4096456?s=400&v=4',
+        nomeDoProjeto: 'Marco Bruno Flix',
+        url: 'https://twitter.com/marcobrunodev/',
+        tags: [
+          'front end',
+          'filmes',
+          'música',
+        ],
+      },
+      {
+        nome: 'Juliana Negreiros',
+        redeSocial: 'https://twitter.com/juunegreiros',
+        foto: 'https://avatars0.githubusercontent.com/u/17054729?s=460&u=7f84a845859479f59c18063284260644928f094c&v=4',
+        nomeDoProjeto: 'Juflix',
+        url: 'https://www.alura.com.br/imersao-react',
+        tags: [
+          'front end',
+          'filmes',
+          'música',
+        ],
+      },
+      {
+        nome: 'Mario Souto',
+        foto: 'https://avatars1.githubusercontent.com/u/13791385?s=460&u=6f629f0aa7530d62552d7b746fc106531588f638&v=4',
+        nomeDoProjeto: 'DevSoutinhoFlix',
+        url: 'https://www.netflix.com/',
+        tags: [
+          'front end',
+          'filmes',
+          'música',
+        ],
+      },
+    {
       nome: 'Juan Oaski',
       foto: 'https://avatars2.githubusercontent.com/u/11061630?s=400&v=4',
       nomeDoProjeto: 'JoP Flix',
